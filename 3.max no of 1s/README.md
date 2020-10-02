7e0ecc0a2a0b426ed4154a5a418a8c2ed1fe780e
LoveKumar <lovekumar2017@iiitkottayam.ac.in>
2020-10-02 23:15:14 +0530

    Code

---
+++ 3.max no of 1s/README.md
@@ -0,0 +1,9 @@
+Given the sorted 2d array having 0's and 1's , find the row number which have maximum no 1's
+#hint check column wise to get one in any row and that wil be the max 
+# imput
+  0 1 2 3
+0 0 0 1 1
+1 0 1 1 1
+2 0 0 1 1
+3 1 1 1 1
+# output 3
---
+++ 3.max no of 1s/maxrow.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+const int R = 4;
+const int C = 4;
+void FindMax(int a[R][C])
+    {   int Row=0,i;
+        int j=C-1;
+        for(i=0;i<R;i++)
+        {
+          while((a[i][j]==1) && (j>=0) )
+          {
+              j--;
+              Row = i;
+          }
+        }
+        printf("%d",Row);
+    }
+int main()
+    { 
+      int a[ ][4] = {{0,0,1,1},{1,1,1,1},{0,0,1,1},{1,1,1,1}};
+      FindMax(a);
+      return 0;
+    }

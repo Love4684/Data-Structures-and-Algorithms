12448c9c83d2fb86ad4bc3fe852bba7b3be83de0
LoveKumar <lovekumar2017@iiitkottayam.ac.in>
2020-10-02 21:14:22 +0530

    New

---
+++ 1.greater element right side/gr.el.rt.c
@@ -0,0 +1,23 @@
+/*given an array replace every element by greater element on right side*/
+#include<stdio.h>
+int main(){
+int n=6,i,temp;    
+int a[6]={16,17,4,3,5,2};
+int max;
+max= a[n-1];
+a[n-1]=-1;
+for(i=n-2;i>=0;i--){
+    temp=a[i];
+    if(max>a[i+1])
+        a[i]=max;
+    else
+        a[i]=a[i+1];
+    if(temp>max)
+    max=temp;
+        
+    }
+ for(i=0;i<n;i++){
+     printf("%d",a[i]);
+ }   
+
+}
